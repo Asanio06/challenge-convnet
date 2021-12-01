dffb784eebe252fae3e05868cf176607b8c1def6
Lansana <lansana.diomande@epfedu.fr>
2021-12-01 08:30:20 +0100

    +82% apres 50 epoch

--- main.py
+++ main.py
@@ -77,29 +77,43 @@ if __name__ == "__main__":
 
     num_classes = 3
     input_shape = (100, 100, 3)
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range=0.2,  # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
+    print(X_train)
+    datagen.fit(X_train)
+    print(X_train)
     model = keras.Sequential(
         [
-            layers.Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=input_shape),
+            layers.Conv2D(16, kernel_size=(3, 3), activation="relu", input_shape=input_shape),
             layers.MaxPooling2D(pool_size=(2, 2)),
 
-            layers.Conv2D(64, kernel_size=(3, 3), activation="relu", padding='valid'),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu", padding='valid'),
+            layers.Dropout(0.2),
 
             layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Conv2D(64, kernel_size=(3, 3), activation="relu", padding='valid'),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu", padding='valid'),
 
             layers.Flatten(),
             layers.Dropout(0.2),
-
-            layers.Dense(32, activation="relu"),
             layers.Dense(num_classes, activation="softmax"),
         ]
     )
 
-    opt = Adam(learning_rate=0.01)
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    opt = Adam(learning_rate=0.0001)
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
     model.summary()
-    model.fit(X_train, y_train, batch_size=100, epochs=50, validation_data=(X_test, y_test))
+    model.fit(X_train, y_train, batch_size=100, epochs=500, validation_data=(X_test, y_test))
 
     prediction_probas = model.evaluate(X_test, y_test)
 
